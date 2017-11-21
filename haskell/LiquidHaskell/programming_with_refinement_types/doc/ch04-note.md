d54720f5ac03e5138d68d2ba718215f764d70f53
waddlaw <ingronze@gmail.com>
2017-11-21 11:48:02 +0900

    LH ch04 更新

--- haskell/LiquidHaskell/programming_with_refinement_types/doc/ch04-note.md
+++ haskell/LiquidHaskell/programming_with_refinement_types/doc/ch04-note.md
@@ -4,3 +4,55 @@
 
 `Data.Vector` を `import` すると自動的に [include/Data/Vector.spec](../src/ch04/include/Data/Vector.spec) が作成され、`liquid -i include/ target.hs` と指定しなくても `liquid target.hs` で実行できる。
 
+## vectorSum
+
+以下のように `Refinement Type` を定義した。
+
+```haskell
+{-@ vectorSum :: Vector Int -> Int @-}
+vectorSum :: Vector Int -> Int
+vectorSum vec = go 0 0
+  where
+    go acc i
+      | i < sz = go (acc + (vec ! i)) (i + 1)
+      | otherwise = acc
+    sz = length vec
+```
+
+このようなエラーになる。
+
+```bash
+**** RESULT: UNSAFE ************************************************************
+ example.hs:57:22-36: Error: Liquid Type Mismatch
+
+ 57 |       | i < sz = go (acc + (vec ! i)) (i + 1)
+                           ^^^^^^^^^^^^^^^
+   Inferred type
+     VV : {v : Int | v == acc + ?a && v == ?b}
+
+   not a subtype of Required type
+     VV : {VV : Int | VV < acc && VV >= 0}
+
+   In Context
+     ?b : {?b : Int | ?b == acc + ?a}
+     ?a : Int
+     acc : Int
+
+ example.hs:57:29-35: Error: Liquid Type Mismatch
+
+ 57 |       | i < sz = go (acc + (vec ! i)) (i + 1)
+                                  ^^^^^^^
+
+   Inferred type
+     VV : {v : Int | v <= sz && v >= 0 && v == i}
+
+   not a subtype of Required type
+     VV : {VV : Int | VV >= 0 && VV < vlen vec}
+
+   In Context
+     sz : {sz : Int | sz >= 0 && sz == len vec}
+     vec : {vec : (Vector Int) | 0 <= vlen vec}
+     i : {i : Int | i <= sz && i >= 0}
+```
+
+`!` で利用される `i` は `i < sz` を満たすはずだが、上手く推論できていない感じがする。
--- haskell/LiquidHaskell/programming_with_refinement_types/src/ch04/example.hs
+++ haskell/LiquidHaskell/programming_with_refinement_types/src/ch04/example.hs
@@ -1,7 +1,18 @@
+{-@ LIQUID "--no-termination" @-}
+
 import Data.Vector (Vector, (!), fromList)
 import qualified Data.Vector as V
-import Prelude hiding (head)
---import Language.Haskell.Liquid.Prelude
+import Prelude hiding (abs, head, length)
+import Data.Function (on)
+import Language.Haskell.Liquid.Prelude
+import Data.List (foldl')
+
+-- | Ch03 で定義した abs 関数
+{-@ abs :: Int -> Nat @-}
+abs :: Int -> Int
+abs n
+  | 0 < n = n
+  | otherwise = 0 - n
 
 {-@ type VectorN a N = { v:Vector a | vlen v == N } @-}
 
@@ -49,37 +60,112 @@ safeLookup x i
 -- |
 -- >>> vectorSum (fromList [1 ,-2, 3])
 -- 2
-{-@ vectorSum :: Vector Int -> Int @-}
+{-@ vectorSum :: {v:Vector Int | 0 <= vlen v} -> Int @-}
 vectorSum :: Vector Int -> Int
 vectorSum vec = go 0 0
   where
+    {-@ go :: Int -> {n:Nat | n <= vlen vec} -> Int @-}
     go acc i
       | i < sz = go (acc + (vec ! i)) (i + 1)
       | otherwise = acc
-    sz = length vec
+    sz = V.length vec
 
--- go acc i
---   | i < sz = go (acc + (vec ! i)) (i + 1)
---   | otherwise = acc
---   where sz = length vec
+{-@ vectorSumDec :: {v:Vector Int | 0 < vlen v} -> Int @-}
+vectorSumDec :: Vector Int -> Int
+vectorSumDec vec = go 0 (sz - 1)
+  where
+    {-@ go :: Int -> {n:Nat | n < vlen vec} -> Int @-}
+    go acc i
+      | i == 0 = acc + (vec ! 0)
+      | otherwise = go (acc + (vec ! i)) (i - 1)
+    sz = V.length vec
 
 -- | Exercise 4.4
 -- Error になる (index が range を超えるため)
--- vectorSum' :: Vector Int -> Int
--- vectorSum' vec = go 0 0
---   where
---     go acc i
---       | i <= sz = go (acc + (vec ! i)) (i + 1)
---       | otherwise = acc
---     sz = length vec
+{- UNSAFE
+{-@ vectorSum' :: {v:Vector Int | 0 <= vlen v} -> Int @-}
+vectorSum' :: Vector Int -> Int
+vectorSum' vec = go 0 0
+  where
+    go acc i
+      | i <= sz = go (acc + (vec ! i)) (i + 1)
+      | otherwise = acc
+    sz = V.length vec
+-}
 
 -- | Exercise 4.5
 -- >>> absoluteSum (fromList [1, -2, 3])
 -- 6
--- absoluteSum :: Vector Int -> Int
--- absoluteSum vec = go 0 0
---   where
---     go acc i
---       | i < sz = go (acc + abs (vec ! i)) (i + 1)
---       | otherwise = acc
---     sz = length vec
+{-@ abs :: Int -> Nat @-}
+{-@ absoluteSum :: Vector Int -> Nat @-}
+absoluteSum :: Vector Int -> Int
+absoluteSum vec = go 0 0
+  where
+    {-@ go :: Nat -> {n:Nat | n <= vlen vec} -> Nat @-}
+    go acc i
+      | i < sz = go (acc + abs (vec ! i)) (i + 1)
+      | otherwise = acc
+    sz = V.length vec
+
+-- | Exercise 4.6
+-- otherwise があるため
+
+{-@ loop :: lo:Nat -> hi:{ Nat | lo <= hi } -> a -> (Btwn lo hi -> a -> a) -> a @-}
+loop :: Int -> Int -> a -> (Int -> a -> a) -> a
+loop lo hi base f = go base lo
+  where
+    go acc i
+      | i < hi = go (f i acc) (i + 1)
+      | otherwise = acc
+
+{-@ vectorSum'' :: {v:Vector Int | 0 <= vlen v} -> Int @-}
+vectorSum'' :: Vector Int -> Int
+vectorSum'' vec = loop 0 n 0 body
+  where
+    body i acc = acc + (vec ! i)
+    n = V.length vec
+
+-- | Exercise 4.7
+-- >>> absoluteSum' (fromList [1, -2, 3])
+-- 6
+{-@ absoluteSum' :: Vector Int -> Nat @-}
+absoluteSum' :: Vector Int -> Int
+absoluteSum' vec = loop 0 n 0 body
+  where
+    {-@ body :: {x:Nat | x < vlen vec} -> Nat -> Nat @-}
+    body i acc = acc + abs (vec ! i)
+    n = V.length vec
+
+-- | Exercise 4.8
+-- >>> dotProduct (fromList [1, 2, 3]) (fromList [4, 5, 6])
+-- 32
+-- Ans: V.length x > V.length y の可能性がある
+{-@ dotProduct :: x:Vector Int -> {y:Vector Int | vlen x <= vlen y} -> Int @-}
+dotProduct :: Vector Int -> Vector Int -> Int
+dotProduct x y = loop 0 sz 0 body
+  where
+    body i acc = acc + (x ! i) * (y ! i)
+    sz = V.length x
+
+{-@ dotProduct :: Vector Int -> Vector Int -> Int @-}
+dotProduct' :: Vector Int -> Vector Int -> Int
+dotProduct' x y = loop 0 sz 0 body
+  where
+    body i acc = acc + (x ! i) * (y ! i)
+    -- sz = (min `on` V.length) x y
+    sz = if V.length x > V.length y then V.length y else V.length x
+
+{-@ type SparseN a N = [(Btwn 0 N, a)] @-}
+
+{-@ sparseProduct :: x:Vector _ -> SparseN _ (vlen x) -> _ @-}
+sparseProduct x y = go 0 y
+  where
+    go n [] = n
+    go n ((i,v):y') = go (n + (x!i) * v) y'
+
+{- foldl' が Traversable なので動かないのかも
+{-@ sparseProduct' :: x:Vector _ -> SparseN _ (vlen x) -> _ @-}
+sparseProduct' x y = foldl' body 0 y
+  where
+    body sum (i, v) = sum + (x ! i) * v
+-}
