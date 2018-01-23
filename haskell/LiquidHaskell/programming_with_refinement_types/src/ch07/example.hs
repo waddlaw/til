7151619491c982d2740f97bc3fa87028e2acd527
Shinya Yamaguchi <ingronze@gmail.com>
2018-01-23 15:26:22 +0900

    update

--- haskell/LiquidHaskell/programming_with_refinement_types/src/ch07/example.hs
+++ haskell/LiquidHaskell/programming_with_refinement_types/src/ch07/example.hs
@@ -1,13 +1,60 @@
+import Prelude hiding (map)
+
+data Vector a = V { vDim :: Int
+                  , vElts :: [a]
+                  }
+                  deriving Eq
+
+data Matrix a = M { mRow :: Int
+                  , mCol :: Int
+                  , mElts :: Vector (Vector a)
+                  }
+                  deriving Eq
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+dotProd vx vy = sum (prod xs ys)
+  where
+    prod = zipWith (\x y -> x * y)
+    xs = vElts vx
+    ys = vElts vy
+
+matProd :: (Num a) => Matrix a -> Matrix a -> Matrix a
+matProd (M rx _ xs) (M _ cy ys) = M rx cy elts
+  where
+    elts = for xs $ \xi ->
+             for ys $ \yj ->
+               dotProd xi yj
+
+for :: Vector a -> (a -> b) -> Vector b
+for (V n xs) f = V n (map f xs)
+
 {-@ measure size @-}
 {-@ size :: xs:[a] -> { v:Nat | v = size xs } @-}
 size :: [a] -> Int
 size [] = 0
 size (_:rs) = 1 + size rs
 
+{-@ measure notEmpty @-}
+notEmpty :: [a] -> Bool
+notEmpty [] = False
+notEmpty (_:_) = True
+
 type List a = [a]
 
 {-@ type ListN a N = { v:List a | size v = N} @-}
 {-@ type ListX a X = ListN a { size X } @-}
+
+-- | Exercise 6.1 (Map)
+{-@ map :: (a -> b) -> xs:List a -> List b @-}
+map _ [] = []
+map f (x:xs) = f x : map f xs
+
+{-@ type TRUE = {v:Bool |     v} @-}
+
+-- {-@ prop_map :: List a -> TRUE @-}
+-- prop_map xs = size ys == size xs
+--   where
+--     ys = map id xs
 
 {-@ reverse :: xs:List a -> ListX a xs @-}
 reverse :: [a] -> [a]
@@ -17,3 +64,4 @@ reverse xs = go [] xs
     {-@ decrease go 2 @-}
     go acc [] = acc
     go acc (x:xs) = go (x:acc) xs
+
--- haskell/LiquidHaskell/programming_with_refinement_types/src/ch08/example.hs
+++ haskell/LiquidHaskell/programming_with_refinement_types/src/ch08/example.hs
@@ -1,4 +1,5 @@
-import Data.Set
+import           Data.Set hiding (insert, filter)
+import Prelude hiding (elem, filter, reverse)
 
 {-@ measure empty :: Set a @-}
 {-@ measure singleton :: a -> Set a @-}
@@ -39,6 +40,7 @@ prop_union_assoc x y z = (x `union` (y `union` z)) == (x `union` y) `union` z
 prop_intersection_dist :: (Ord a) => Set a -> Set a -> Set a -> Bool
 prop_intersection_dist x y z = x `intersection` (y `union` z) == (x `intersection` y) `union` (x `intersection` z)
 
+-- | Exercise 7.2
 {- TODO
 {-@ prop_cup_dif_bad :: _ -> _ -> True @-}
 prop_cup_dif_bad :: (Ord a) => Set a -> Set a -> Bool
@@ -49,7 +51,7 @@ prop_cup_dif_bad x y = pre `implies` (x == ((x `union` y) `difference` y))
 
 {-@ measure elts @-}
 elts :: (Ord a) => [a] -> Set a
-elts [] = empty
+elts []     = empty
 elts (x:xs) = singleton x `union` elts xs
 
 {-@ type ListS a S = {v:[a] | elts v = S} @-}
@@ -61,7 +63,7 @@ elts (x:xs) = singleton x `union` elts xs
 
 {-@ append' :: xs:_ -> ys:_ -> ListUn a xs ys @-}
 append' :: [a] -> [a] -> [a]
-append' [] ys = ys
+append' [] ys     = ys
 append' (x:xs) ys = x : append' xs ys
 
 -- | Exercise 7.3 (Reverse)
@@ -73,3 +75,127 @@ reverse' xs = revHelper [] xs
 revHelper :: [a] -> [a] -> [a]
 revHelper acc []     = acc
 revHelper acc (x:xs) = revHelper (x:acc) xs
+
+-- -- | Exercise 7.4
+-- halve :: Int -> [a] -> ([a], [a])
+-- halve 0 xs       = ([], xs)
+-- halve n (x:y:zs) = (x:xs, y:ys)
+--   where
+--     (xs, ys) = halve (n-1) zs
+-- halve _ xs = ([], xs)
+
+-- {-@ prop_halve_append :: _ -> _ -> True @-}
+-- prop_halve_append n xs = elts xs == elts xs'
+--   where
+--     xs' = append' ys zs
+--     (ys, zs) = halve n xs
+
+-- | Exercise 7.5
+{-@ elem :: (Eq a) => x:a -> xs:[a] -> { v:Bool | v <=> member x (elts xs) } @-}
+elem :: Eq a => a -> [a] -> Bool
+elem x (y:ys) = x == y || elem x ys
+elem _ [] = False
+
+{-@ test1 :: True @-}
+test1 :: Bool
+test1 = elem 2 [1,2,3]
+
+{-@ test2 :: False @-}
+test2 :: Bool
+test2 = elem 2 [1,3]
+
+{-@ insert :: x:a -> xs:[a] -> ListUn1 a x xs @-}
+insert :: (Ord a) => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys)
+  | x <= y = x : y :ys
+  | otherwise = y : insert x ys
+
+{-@ insertSort :: (Ord a) => xs:[a] -> ListEq a xs @-}
+insertSort :: (Ord a) => [a] -> [a]
+insertSort [] = []
+insertSort (x:xs) = insert x (insertSort xs)
+
+-- | Exercise 7.6 (Merge)
+-- {-@ merge :: xs:[a] -> ys:[a] -> [a] @-}
+-- merge (x:xs) (y:ys)
+--   | x <= y = x : merge xs (y:ys)
+--   | otherwise = y : merge (x:xs) ys
+-- merge [] ys = ys
+-- merge xs [] = xs
+
+-- {-@ prop_merge_app :: _ -> _ -> True @-}
+-- prop_merge_app xs ys = elts zs == elts zs'
+--   where
+--     zs = append' xs ys
+--     zs' = merge xs ys
+
+-- | Exercise 7.7 (Merge Sort)
+-- {-@ mergeSort :: (Ord a) => xs:[a] -> ListEmp a @-}
+-- mergeSort [] = []
+-- mergeSort xs = merge (mergeSort ys) (mergeSort zs)
+--   where
+--     (ys, zs) = halve mid xs
+--     mid = length xs `div` 2
+
+{-@ measure unique @-}
+unique :: (Ord a) => [a] -> Bool
+unique [] = True
+unique (x:xs) = unique xs && not (member x (elts xs))
+
+{-@ type UList a = { v:[a] | unique v} @-}
+
+{-@ isUnique :: UList Int @-}
+isUnique :: [Int]
+isUnique = [1,2,3]
+
+-- UNSAFE {-@ isNotUnique :: UList Int @-}
+isNotUnique :: [Int]
+isNotUnique = [1,2,3,1]
+
+{-@ filter :: (a -> Bool) -> xs:UList a -> { v:ListSub a xs | unique v} @-}
+filter _ [] = []
+filter f (x:xs)
+  | f x = x:xs'
+  | otherwise = xs'
+  where
+    xs' = filter f xs
+
+-- | Exercise 7.8 (Filter)
+-- filter' _ [] = []
+-- filter' f (x:xs)
+--   | f x = x : xs'
+--   | otherwise = xs'
+--   where
+--     xs' = filter' f xs
+
+-- {-@ test3 :: UList _ @-}
+-- test3 = filter' (> 2) [1,2,3,4]
+
+-- {-@ test4 :: [_] @-}
+-- test4 = filter' (> 3) [3,1,2,3]
+
+-- | Exercise 7.9 (Reverse)
+{-@ reverse :: xs:UList a -> UList a @-}
+reverse = go []
+  where
+    {-@ go :: a:[a] -> xs:UList a -> UList a @-}
+    go a [] = a
+    go a (x:xs) = go (x:a) xs
+
+-- {-@ nub :: [a] -> UList a @-}
+-- nub xs = go [] xs
+--   where
+--     go seen [] = seen
+--     go seen (x:xs)
+--       | x `isin` seen = go seen xs
+--       | otherwise = go (x:seen) xs
+
+-- -- FIXME
+-- {-@ predicate In X Xs = Set_mem X (elts Xs) @-}
+
+-- {-@ isin :: x:_ -> ys:_ -> { v:Bool | v <=> In x ys } @-}
+-- isin x (y:ys)
+--   | x == y = True
+--   | otherwise = x `isin` ys
+-- isin _ [] = False
