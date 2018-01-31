0b5ecc6c564ecba1171b1f0ec6cc3d0cce16ac34
waddlaw <ingronze@gmail.com>
2018-01-31 12:16:07 +0900

    update

--- haskell/LiquidHaskell/programming_with_refinement_types/src/ch04/example.hs
+++ haskell/LiquidHaskell/programming_with_refinement_types/src/ch04/example.hs
@@ -1,11 +1,11 @@
 {-@ LIQUID "--no-termination" @-}
-
+{-@ LIQUID "--short-names"         @-}
+{-@ LIQUID "--scrape-used-imports" @-}
+import Data.Function (on)
+import Data.List (foldl')
 import Data.Vector (Vector, (!), fromList)
 import qualified Data.Vector as V
 import Prelude hiding (abs, head, length)
-import Data.Function (on)
-import Language.Haskell.Liquid.Prelude
-import Data.List (foldl')
 
 -- | Ch03 で定義した abs 関数
 {-@ abs :: Int -> Nat @-}
@@ -15,42 +15,41 @@ abs n
   | otherwise = 0 - n
 
 {-@ type VectorN a N = { v:Vector a | vlen v == N } @-}
-
 {-@ twoLangs :: VectorN String 2 @-}
+twoLangs :: Vector String
 twoLangs = fromList ["haskell", "javascript"]
 
 {- UNSAFE
+eeks :: [String]
 eeks = [ok, yup, nono]
   where
     ok = twoLangs ! 0
     yup = twoLangs ! 1
     nono = twoLangs ! 3
 -}
-
 {-@ type NEVector a = { v:Vector a | 0 < vlen v} @-}
-
 {-@ head :: NEVector a -> a @-}
 head :: Vector a -> a
 head vec = vec ! 0
 
 -- | Exercise 4.1
+{-@ head' :: Vector a -> Maybe a @-}
 head' :: Vector a -> Maybe a
 head' vec
   | null vec = Nothing
   | otherwise = Just $ V.head vec
 
 {-@ type Btwn Lo Hi = { v:Int | Lo <= v && v < Hi} @-}
-
 -- | Exercise 4.2
 -- (!) :: Vector a -> Int -> a
 -- assume ! :: x:Vector a -> vec:{v:Nat | v < vlen x } -> a
-
-{-@ unsafeLookup :: x:Nat -> {v:Vector a | x == vlen v && vlen v < x} -> a @-}
+{-@ unsafeLookup :: x:Nat -> {v:Vector a | x < vlen v} -> a @-}
 unsafeLookup :: Int -> Vector a -> a
 unsafeLookup index vec = vec ! index
 
 -- | Exercise 4.3
 {-@ safeLookup :: Vector a -> Int -> Maybe a @-}
+safeLookup :: Vector a -> Int -> Maybe a
 safeLookup x i
   | ok = Just (x ! i)
   | otherwise = Nothing
@@ -63,8 +62,8 @@ safeLookup x i
 {-@ vectorSum :: {v:Vector Int | 0 <= vlen v} -> Int @-}
 vectorSum :: Vector Int -> Int
 vectorSum vec = go 0 0
-  where
     {-@ go :: Int -> {n:Nat | n <= vlen vec} -> Int @-}
+  where
     go acc i
       | i < sz = go (acc + (vec ! i)) (i + 1)
       | otherwise = acc
@@ -73,8 +72,8 @@ vectorSum vec = go 0 0
 {-@ vectorSumDec :: {v:Vector Int | 0 < vlen v} -> Int @-}
 vectorSumDec :: Vector Int -> Int
 vectorSumDec vec = go 0 (sz - 1)
-  where
     {-@ go :: Int -> {n:Nat | n < vlen vec} -> Int @-}
+  where
     go acc i
       | i == 0 = acc + (vec ! 0)
       | otherwise = go (acc + (vec ! i)) (i - 1)
@@ -92,16 +91,14 @@ vectorSum' vec = go 0 0
       | otherwise = acc
     sz = V.length vec
 -}
-
 -- | Exercise 4.5
 -- >>> absoluteSum (fromList [1, -2, 3])
 -- 6
-{-@ abs :: Int -> Nat @-}
 {-@ absoluteSum :: Vector Int -> Nat @-}
 absoluteSum :: Vector Int -> Int
 absoluteSum vec = go 0 0
-  where
     {-@ go :: Nat -> {n:Nat | n <= vlen vec} -> Nat @-}
+  where
     go acc i
       | i < sz = go (acc + abs (vec ! i)) (i + 1)
       | otherwise = acc
@@ -109,7 +106,6 @@ absoluteSum vec = go 0 0
 
 -- | Exercise 4.6
 -- otherwise があるため
-
 {-@ loop :: lo:Nat -> hi:{ Nat | lo <= hi } -> a -> (Btwn lo hi -> a -> a) -> a @-}
 loop :: Int -> Int -> a -> (Int -> a -> a) -> a
 loop lo hi base f = go base lo
@@ -131,8 +127,8 @@ vectorSum'' vec = loop 0 n 0 body
 {-@ absoluteSum' :: Vector Int -> Nat @-}
 absoluteSum' :: Vector Int -> Int
 absoluteSum' vec = loop 0 n 0 body
-  where
     {-@ body :: {x:Nat | x < vlen vec} -> Nat -> Nat @-}
+  where
     body i acc = acc + abs (vec ! i)
     n = V.length vec
 
@@ -140,30 +136,32 @@ absoluteSum' vec = loop 0 n 0 body
 -- >>> dotProduct (fromList [1, 2, 3]) (fromList [4, 5, 6])
 -- 32
 -- Ans: V.length x > V.length y の可能性がある
-{-@ dotProduct :: x:Vector Int -> {y:Vector Int | vlen x <= vlen y} -> Int @-}
+{-@ dotProduct :: x:Vector Int -> {y:Vector Int | vlen x == vlen y} -> Int @-}
 dotProduct :: Vector Int -> Vector Int -> Int
 dotProduct x y = loop 0 sz 0 body
   where
     body i acc = acc + (x ! i) * (y ! i)
     sz = V.length x
 
-{-@ dotProduct :: Vector Int -> Vector Int -> Int @-}
+{-@ dotProduct' :: Vector Int -> Vector Int -> Int @-}
 dotProduct' :: Vector Int -> Vector Int -> Int
-dotProduct' x y = loop 0 sz 0 body
+dotProduct' x y
+  | V.length x == V.length y = loop 0 sz 0 body
+  | otherwise = error "length mismatch"
   where
     body i acc = acc + (x ! i) * (y ! i)
     -- sz = (min `on` V.length) x y
-    sz = if V.length x > V.length y then V.length y else V.length x
+    sz =
+      if V.length x > V.length y
+        then V.length y
+        else V.length x
 
 {-@ type SparseN a N = [(Btwn 0 N, a)] @-}
-
 {-@ sparseProduct :: x:Vector _ -> SparseN _ (vlen x) -> _ @-}
 sparseProduct x y = go 0 y
   where
     go n [] = n
-    go n ((i,v):y') = go (n + (x!i) * v) y'
-
-{- foldl' が Traversable なので動かないのかも
+    go n ((i, v):y') = go (n + (x ! i) * v) y' {- foldl' が Traversable なので動かないのかも
 {-@ sparseProduct' :: x:Vector _ -> SparseN _ (vlen x) -> _ @-}
 sparseProduct' x y = foldl' body 0 y
   where
