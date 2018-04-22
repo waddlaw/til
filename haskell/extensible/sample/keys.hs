0cb4dd8905064e5dacdcb5f68ecd08fd5e4f4bfc
Shinya Yamaguchi <waddlaw@users.noreply.github.com>
2018-04-22 14:14:20 +0900

    Create keys.hs

---
+++ haskell/extensible/sample/keys.hs
@@ -0,0 +1,34 @@
+#!/usr/bin/env stack
+-- stack script --resolver nightly-2018-04-21 --package extensible -- keys.hs
+
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import Data.Extensible
+import Data.Proxy
+import GHC.TypeLits (KnownSymbol, symbolVal)
+
+keys :: Forall (KeyIs KnownSymbol) xs => proxy xs -> [String]
+keys xs = henumerateFor (Proxy @ (KeyIs KnownSymbol)) xs
+  ((:) . symbolVal . proxyAssocKey) []
+
+data Person = Person
+  { name  :: String
+  , age   :: Int
+  } deriving Show
+
+deriveIsRecord ''Person
+
+type PersonRecord = Record '["name" ':> String, "age" ':> Int]
+
+p1 :: PersonRecord
+p1 = toRecord $ Person "guchi" 0
+
+main :: IO ()
+main = print $ keys p1
